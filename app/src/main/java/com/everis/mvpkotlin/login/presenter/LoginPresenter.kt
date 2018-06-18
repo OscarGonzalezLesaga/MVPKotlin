03948b9cfb6b55515580ca6a9db0b2a3da32e471
Oscar Gonzalez Lesaga <oscar.gonzalez.lesaga@everis.com>
2018-06-18 10:53:57 +0200

    create view login with constraint layout

--- app/src/main/java/com/everis/mvpkotlin/login/presenter/LoginPresenter.kt
+++ app/src/main/java/com/everis/mvpkotlin/login/presenter/LoginPresenter.kt
@@ -2,6 +2,6 @@ package com.everis.mvpkotlin.login.presenter
 
 class LoginPresenter : ILoginPresenter {
     override fun manageLoginUser(user: String, password: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
--- app/src/main/java/com/everis/mvpkotlin/login/view/LoginActivity.kt
+++ app/src/main/java/com/everis/mvpkotlin/login/view/LoginActivity.kt
@@ -9,23 +9,18 @@ class LoginActivity : BaseActivity(), LoginView {
         get() = R.layout.activity_login
 
     override fun linkView() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun linkPresenter() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun logic() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun showErrorLogin() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun manageLoginOk() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
