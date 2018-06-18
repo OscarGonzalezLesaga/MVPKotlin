48df5f8e68e3bddcbd5afddae697e4356c5015c9
Oscar Gonzalez Lesaga <oscar.gonzalez.lesaga@everis.com>
2018-06-18 17:45:42 +0200

    link view viewHolder with data recyclerview

--- app/src/main/java/com/everis/mvpkotlin/listEmployees/view/adapter/EmployeesAdapter.kt
+++ app/src/main/java/com/everis/mvpkotlin/listEmployees/view/adapter/EmployeesAdapter.kt
@@ -1,12 +1,14 @@
 package com.everis.mvpkotlin.listEmployees.view.adapter
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.everis.mvpkotlin.R
 import com.everis.mvpkotlin.listEmployees.model.entities.Employee
+import kotlinx.android.synthetic.main.item_employee_list.view.*
 
 class EmployeesAdapter(var employeeList: List<Employee> = ArrayList(), var context: Context) : RecyclerView.Adapter<EmployeesAdapter.EmployeeViewHolder>() {
 
@@ -20,13 +22,23 @@ class EmployeesAdapter(var employeeList: List<Employee> = ArrayList(), var conte
     }
 
     override fun onBindViewHolder(holder: EmployeeViewHolder, position: Int) {
-        holder.bind(employeeList[position])
+
+        var backgroundColor = if (position % 2 == 0) {
+            ContextCompat.getColor(context, R.color.colorPrimaryTransparent)
+        } else {
+            ContextCompat.getColor(context, R.color.white)
+        }
+
+        holder.bind(employeeList[position], backgroundColor)
     }
 
     class EmployeeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        fun bind(employee: Employee) {
-
+        fun bind(employee: Employee, backgroundColor: Int) {
+            itemView.nameText.text = employee?.name
+            itemView.emailText.text = employee?.email
+            itemView.usernameText.text = employee?.username
+            itemView.setBackgroundColor(backgroundColor)
         }
     }
 }
